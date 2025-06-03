cda81d301ff984729b0027eb1dade2dbed22cf1a
Student <icep.memory1@gmail.com>
2025-06-03 09:18:33 +0200

    update

--- routes/student.js
+++ routes/student.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 
-router.post('/uploadFile',upload.single('file'), (req, res)=>{    
+router.post('/uploadFile', upload.single('file'), (req, res) => {
     var filepath = req.file.path;
     const filestream = fs.createReadStream(filepath);
     filestream.on('error', (err) => {
@@ -27,49 +27,57 @@ router.post('/uploadFile',upload.single('file'), (req, res)=>{
     })
     const params = {
         Bucket: 'www.icep.co.za',
-        Key: `CVs/${Date.now().toString()}`+req.file.originalname,
+        Key: `CVs/${Date.now().toString()}` + req.file.originalname,
         Body: filestream
     }
-    
+
     s3.upload(params, (err, data) => {
         if (err) { console.log(err); }
-        if (data) {
-            var link = data.Location
-            res.send({ success: true, message: "Uploaded successfully", link });
+        try {
+            if (data) {
+                var link = data.Location
+                res.send({ success: true, message: "Uploaded successfully", link });
+            }
+        } catch (error) {
+            res.json({ success: false, message: "Unable to upload file",error });
         }
-        else{
-            res.send({ success: false, message: "Unable to upload file", err });
-        }
+        // if (data) {
+        //     var link = data.Location
+        //     res.send({ success: true, message: "Uploaded successfully", link });
+        // }
+        // else{
+        //     res.send({ success: false, message: "Unable to upload file" });
+        // }
     })
-    
+
 })
 
-router.get('/getCampuses', (req, res) =>{
-    connection.query('select * from campus where open = true', (err, results)=>{
-        if(err){
+router.get('/getCampuses', (req, res) => {
+    connection.query('select * from campus where open = true', (err, results) => {
+        if (err) {
             console.log(err);
             return
         }
-        if(results.length> 0){
-            res.send({success: true, results})
+        if (results.length > 0) {
+            res.send({ success: true, results })
         }
-        else{
-            res.send({success: false, message:'This application is not open for any campuses'})
+        else {
+            res.send({ success: false, message: 'This application is not open for any campuses' })
         }
     })
 })
 
-router.get('/getCourse', (req, res) =>{
-    connection.query('select * from course where open = true', (err, results)=>{
-        if(err){
+router.get('/getCourse', (req, res) => {
+    connection.query('select * from course where open = true', (err, results) => {
+        if (err) {
             console.log(err);
             return
         }
-        if(results.length> 0){
-            res.send({success: true, results})
+        if (results.length > 0) {
+            res.send({ success: true, results })
         }
-        else{
-            res.send({success: false, message:'This application is not open for any courses'})
+        else {
+            res.send({ success: false, message: 'This application is not open for any courses' })
         }
     })
 })
@@ -82,41 +90,41 @@ router.post('/student_application', (req, res) => {
                         // and post_ref ='${req.body.post_ref}' */
     connection.query(`select * from  student
                         where student_id = '${req.body.student_id}'`
-                        // and post_ref ='${req.body.post_ref}'
-                        , (error, result) => {
-        if (error) throw error;
-        if (result.length > 0) {
-            res.send({ success: false, message: "this student number already exist in this application", result });
-        }
-        else {
-            var sqlInsert = `INSERT INTO student (student_id, firstname, lastname, email, idno, dob, phoneNo, gender, outstanding, houseNo, streetName, town, code, cv_file, 
+        // and post_ref ='${req.body.post_ref}'
+        , (error, result) => {
+            if (error) throw error;
+            if (result.length > 0) {
+                res.send({ success: false, message: "this student number already exist in this application", result });
+            }
+            else {
+                var sqlInsert = `INSERT INTO student (student_id, firstname, lastname, email, idno, dob, phoneNo, gender, outstanding, houseNo, streetName, town, code, cv_file, 
                                 recommendation_file, course_id, campus_id, status)
                                 values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
 
-            var bodyParams = [req.body.student_id, req.body.firstname, req.body.lastname, req.body.email, req.body.idno, req.body.dob, req.body.phoneNo, req.body.gender,
-            req.body.outstanding, req.body.houseNo, req.body.streetName, req.body.town, req.body.code, req.body.cv_file, req.body.recommendation_file, req.body.course, req.body.campus, "Pending"];
+                var bodyParams = [req.body.student_id, req.body.firstname, req.body.lastname, req.body.email, req.body.idno, req.body.dob, req.body.phoneNo, req.body.gender,
+                req.body.outstanding, req.body.houseNo, req.body.streetName, req.body.town, req.body.code, req.body.cv_file, req.body.recommendation_file, req.body.course, req.body.campus, "Pending"];
 
-            connection.query(sqlInsert, bodyParams, (err, results) => {
-                if (err) throw err;
+                connection.query(sqlInsert, bodyParams, (err, results) => {
+                    if (err) throw err;
 
-                if (results.affectedRows != 0) {
-                    res.send({ success: true, message: "The request was send successfully" });
-                }
-                else {
-                    res.send({ success: false, message: "Unable to send application" });
-                }
-            })
-        }
-    })
-    
+                    if (results.affectedRows != 0) {
+                        res.send({ success: true, message: "The request was send successfully" });
+                    }
+                    else {
+                        res.send({ success: false, message: "Unable to send application" });
+                    }
+                })
+            }
+        })
+
 })
 
-router.get('/check_status/:student_no', (req, res)=>{
+router.get('/check_status/:student_no', (req, res) => {
 
-    connection.query(`select * from student where student_id = '${req.params.student_no}'`, (err, results)=>{
-        if(err){console.log(err); return}
-        if(results.length>0){res.send({success:true, results})}
-        else{res.send({success:false, message:'No data has found'})}
+    connection.query(`select * from student where student_id = '${req.params.student_no}'`, (err, results) => {
+        if (err) { console.log(err); return }
+        if (results.length > 0) { res.send({ success: true, results }) }
+        else { res.send({ success: false, message: 'No data has found' }) }
     })
 })
 
